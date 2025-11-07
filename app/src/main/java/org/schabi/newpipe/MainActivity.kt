c988818d31eaf0270ca7d036193e19ea8e5ee2fb
Ernest Pease <peaseernest8@gmail.com>
2025-11-07 07:01:45 +0300

    added buttons and auto update hope it worksv7

--- app/src/main/java/org/schabi/newpipe/MainActivity.kt
+++ app/src/main/java/org/schabi/newpipe/MainActivity.kt
@@ -4,25 +4,16 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
-import org.schabi.newpipe.about.AboutActivity
 import org.schabi.newpipe.databinding.ActivityMainBinding
-import org.schabi.newpipe.fragments.ExploreFragment
-import org.schabi.newpipe.fragments.LibraryFragment
-import org.schabi.newpipe.util.ThemeHelper
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(ThemeHelper.getSettingsThemeStyle(this))
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -30,77 +21,19 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowTitleEnabled(true)
 
-        drawerLayout = binding.drawerLayout
-        val toggle = ActionBarDrawerToggle(
-            this, drawerLayout, binding.toolbar,
-            R.string.drawer_open, R.string.drawer_close
-        )
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-
         binding.navigationView.setNavigationItemSelectedListener(this)
-
-        setupViewPager()
-    }
-
-    private fun setupViewPager() {
-        binding.viewPager2.adapter = object : FragmentStateAdapter(this) {
-            override fun getItemCount() = 2
-            override fun createFragment(position: Int): Fragment {
-                return when (position) {
-                    0 -> ExploreFragment()
-                    1 -> LibraryFragment()
-                    else -> throw IllegalStateException("Unexpected position $position")
-                }
-            }
-        }
-
-        TabLayoutMediator(binding.tabLayout, binding.viewPager2) { tab, position ->
-            tab.text = when (position) {
-                0 -> getString(R.string.tab_explore)
-                1 -> getString(R.string.tab_library)
-                else -> ""
-            }
-        }.attach()
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == android.R.id.home) {
-            finish()
-            return true
-        }
-        return super.onOptionsItemSelected(item)
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.nav_about -> {
-                startActivity(Intent(this, AboutActivity::class.java))
-                return true
-            }
-            R.id.nav_telegram -> {
-                openUrl(getString(R.string.telegram_channel_url))
-                return true
-            }
-            R.id.nav_whatsapp -> {
-                openUrl(getString(R.string.whatsapp_channel_url))
-                return true
-            }
+            R.id.nav_telegram -> openUrl(getString(R.string.telegram_channel_url))
+            R.id.nav_whatsapp -> openUrl(getString(R.string.whatsapp_channel_url))
         }
-        drawerLayout.closeDrawer(GravityCompat.START)
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
         return true
     }
 
     private fun openUrl(url: String) {
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-        startActivity(intent)
-    }
-
-    override fun onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
+        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
     }
 }
